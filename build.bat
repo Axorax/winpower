3dbc16e7c8c4dc3dfc5b0131789f3808f1610b06
Axorax <mrunknownboy12345@gmail.com>
2024-09-22 15:05:08 +0600

    Winpower

---
+++ build.bat
@@ -0,0 +1,2 @@
+@echo off
+pyinstaller --name="Winpower" --onefile --paths=env/Lib/site-packages --add-data="env/Lib/site-packages/sv_ttk;sv_ttk" --add-data="icon.ico;." --noconsole --icon=icon.png main.py
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pyinstaller --name="Winpower" --onefile --paths=env/Lib/site-packages --add-data="env/Lib/site-packages/sv_ttk:sv_ttk" --add-data="icon.ico:." --noconsole --icon=icon.png main.py
